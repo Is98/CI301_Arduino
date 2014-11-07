621dda6a21c162591bdff81530b6db17a0f9a671
Is98 <contact@simstop112.co.uk>
2014-11-07 13:03:23 +0000

    Modular SQL injection and fixed log SQL

--- CI301/CI301.ino
+++ CI301/CI301.ino
@@ -1,4 +1,5 @@
 
+#include <stdarg.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <dht.h>
@@ -82,110 +83,143 @@ void setup() {
 
 
 void loop() {
+  
+    WebServer();
+    logReadings();
+}
+  
+
+
+
+
+
+void sendReadings(int sensornum, double humidity, double temp) { // return 1 if successful
+  String SQL_READINGS = "";
+  
+  char SQL_SEND_READINGS[100];
+  sprintf(SQL_SEND_READINGS, "INSERT INTO iansmi9_ard.log VALUES (NULL, CURRENT_TIMESTAMP, '%d', '%f', '%f')", sensornum, humidity, temp);
+  
+  if (my_conn.mysql_connect(addr_mysql, 3306, user, password))
+  {
+    delay(500);
+    /* run the SQL query */
+    my_conn.cmd_query(SQL_SEND_READINGS);
+    Serial.println("Query Success!");
+  } 
+  else  {
+    Serial.println("Connection failed.");
+  }
+}
+
+
+
+
+
+void WebServer() {
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
-    Serial.println("New client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 60");  // refresh the page automatically every 5 sec
-          client.println();
-          //and metadata...
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<head>");
-          client.println("<title>Environment Live Times</title>");
-          //and CSS style data...
-          client.println("<style type=\"text/css\">");
-          client.println("container {");
-          client.println("position: relative;");
-          client.println("width: 400px;");
-          client.println("margin-left:auto;");
-          client.println("margin-right:auto;");
-          client.println("}");
-          client.println("reading {");
-          client.println("float: left;");
-          client.println("width: 100px;");
-          client.println("font-family: \"MS Serif\", \"New York\", serif;");
-          client.println("font-size: 12px;");
-          client.println("text-align: center;");
-          client.println("padding: 20px, 20px. 20px, 20px;");
-          client.println("}");
-          client.println("reading h1 {");
-          client.println("font-family: Georgia, \"Times New Roman\", Times, serif;");
-          client.println("font-size: 14px;");
-          client.println("font-style: oblique;");
-          client.println("}");
+  Serial.println("New client");
+  // an http request ends with a blank line
+  boolean currentLineIsBlank = true;
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+      // if you've gotten to the end of the line (received a newline
+      // character) and the line is blank, the http request has ended,
+      // so you can send a reply
+      if (c == '\n' && currentLineIsBlank) {
+        // send a standard http response header
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-Type: text/html");
+        client.println("Connection: close");  // the connection will be closed after completion of the response
+        client.println("Refresh: 60");  // refresh the page automatically every 5 sec
+        client.println();
+        //and metadata...
+        client.println("<!DOCTYPE HTML>");
+        client.println("<html>");
+        client.println("<head>");
+        client.println("<title>Environment Live Times</title>");
+        //and CSS style data...
+        client.println("<style type=\"text/css\">");
+        client.println("container {");
+        client.println("position: relative;");
+        client.println("width: 400px;");
+        client.println("margin-left:auto;");
+        client.println("margin-right:auto;");
+        client.println("}");
+        client.println("reading {");
+        client.println("float: left;");
+        client.println("width: 100px;");
+        client.println("font-family: \"MS Serif\", \"New York\", serif;");
+        client.println("font-size: 12px;");
+        client.println("text-align: center;");
+        client.println("padding: 20px, 20px. 20px, 20px;");
+        client.println("}");
+        client.println("reading h1 {");
+        client.println("font-family: Georgia, \"Times New Roman\", Times, serif;");
+        client.println("font-size: 14px;");
+        client.println("font-style: oblique;");
+        client.println("}");
 
-          client.println("</style>");
-          client.println("</head>");
-          //and body content.
-          client.println("<body>");
-          client.println("<container>");
+        client.println("</style>");
+        client.println("</head>");
+        //and body content.
+        client.println("<body>");
+        client.println("<container>");
 
-          // output the value of each sensor
+        // output the value of each sensor
 
-          DHT.read11(dht1);
-          client.print("<reading><h1>");
-          client.print(dht1NN);
-          client.print("</h1> <br /> Humidity <br /> ");
-          client.print(DHT.humidity);
-          client.print("%.  <br /><br />   Temperature <br /> ");
-          client.print(DHT.temperature);
-          client.println("C </reading>");      
+        DHT.read11(dht1);
+        client.print("<reading><h1>");
+        client.print(dht1NN);
+        client.print("</h1> <br /> Humidity <br /> ");
+        client.print(DHT.humidity);
+        client.print("%.  <br /><br />   Temperature <br /> ");
+        client.print(DHT.temperature);
+        client.println("C </reading>");   
 
-          DHT.read11(dht2);
-          client.print("<reading><h1>");
-          client.print(dht2NN);
-          client.print("</h1> <br /> Humidity <br /> ");
-          client.print(DHT.humidity);
-          client.print("%.  <br /><br />   Temperature <br /> ");
-          client.print(DHT.temperature);
-          client.println("C </reading>");    
 
-          DHT.read11(dht3);
-          client.print("<reading><h1>");
-          client.print(dht3NN);
-          client.print("</h1> <br /> Humidity <br /> ");
-          client.print(DHT.humidity);
-          client.print("%.  <br /><br />   Temperature <br /> ");
-          client.print(DHT.temperature);
-          client.println("C </reading>");    
+        DHT.read11(dht2);
+        client.print("<reading><h1>");
+        client.print(dht2NN);
+        client.print("</h1> <br /> Humidity <br /> ");
+        client.print(DHT.humidity);
+        client.print("%.  <br /><br />   Temperature <br /> ");
+        client.print(DHT.temperature);
+        client.println("C </reading>");    
 
-          DHT.read11(dht4);
-          client.print("<reading><h1>");
-          client.print(dht4NN);
-          client.print("</h1> <br /> Humidity <br /> ");
-          client.print(DHT.humidity);
-          client.print("%.  <br /><br />   Temperature <br /> ");
-          client.print(DHT.temperature);
-          client.println("C </reading>");    
-          client.println("</container>");
+        DHT.read11(dht3);
+        client.print("<reading><h1>");
+        client.print(dht3NN);
+        client.print("</h1> <br /> Humidity <br /> ");
+        client.print(DHT.humidity);
+        client.print("%.  <br /><br />   Temperature <br /> ");
+        client.print(DHT.temperature);
+        client.println("C </reading>");    
 
-          client.println("</body>");
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
+        DHT.read11(dht4);
+        client.print("<reading><h1>");
+        client.print(dht4NN);
+        client.print("</h1> <br /> Humidity <br /> ");
+        client.print(DHT.humidity);
+        client.print("%.  <br /><br />   Temperature <br /> ");
+        client.print(DHT.temperature);
+        client.println("C </reading>");    
+        client.println("</container>");
+
+        client.println("</body>");
+        client.println("</html>");
+        break;
+      }
+      if (c == '\n') {
+        // you're starting a new line
+        currentLineIsBlank = true;
+      } 
+      else if (c != '\r') {
+        // you've gotten a character on the current line
+        currentLineIsBlank = false;
       }
     }
     // give the web browser time to receive the data
@@ -194,35 +228,12 @@ void loop() {
     client.stop();
     Serial.println("client disonnected");
   }
-}
-
-boolean sendReadings(int sensornum, double temp, double humidity) { // return 1 if successful
-
-  char SQL_SEND_READINGS[] = ("");
-  if (my_conn.mysql_connect(addr_mysql, 3306, user, password))
-  {
-    delay(500);
-    /* run the SQL query */
-    my_conn.cmd_query(SQL_SEND_READINGS);
-    Serial.println("Query Success!");
-    i = 0;
-  } 
-  else  {
-    Serial.println("Connection failed.");
   }
-
-
 }
 
 
-
-
-
-
-
-
-
-
+void logReadings() {
+}
 
 
 
