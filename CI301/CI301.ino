c66e9dc2277f706863d88a6b773877472a163ebd
Is98 <contact@simstop112.co.uk>
2014-11-15 13:35:13 +0000

    Revert 638865b..31bc1e2
    This rolls back to commit 638865b008f80f3e5b63ef341456d7cb799b30e4.

--- CI301/CI301.ino
+++ CI301/CI301.ino
@@ -7,24 +7,24 @@
 #include "mysql.h"
 
 //Pin references 
-int pin_relay[] = {2,3,4,5};
-int pin_dht[] = {54,55,56,57}; //no ; here. Set equal to channel sensor is on {A0,A1,A2,A3}
+int pin_relay[] = {
+  2,3,4,5};
+int pin_dht[] = {
+  54,55,56,57}; //no ; here. Set equal to channel sensor is on {A0,A1,A2,A3}
 
 //Sensor nicknames
-String dhtNN[] = { "Ambient Soil", "Outer Hood", "Plant Top", "Plant Center"};
+String dhtNN[] = { 
+  "Ambient Soil", "Outer Hood", "Plant Top", "Plant Center"};
 
 dht DHT;
 
-int scheduler = 0;
-int previousScheduler = 4;
 int web_priority = 25000; // 10000 is around 13 seconds. Max 32750 = 40 seconds
 
 int relay1_on_temp = 23;
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress addr_me(192,168,0,66);
 IPAddress addr_mysql(198,23,57,27);
 
@@ -43,16 +43,16 @@ EthernetServer server(66);
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  
+
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
+
   for (int j=0; j < 4; j++) {
     pinMode(pin_dht[j], INPUT);
     pinMode(pin_relay[j], OUTPUT);
   }
-  
+
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, addr_me);
   server.begin();
@@ -76,18 +76,9 @@ void setup() {
 
 void loop() {
 
-  switch (scheduler) {
-
-  case 0:
     WebServer();
-    break;
-    
-  default:
-    DHT.read11(pin_dht[scheduler - 1]); //arrays start at [0], scheduler starts at 1
-    sendReadings(scheduler - 1, DHT.humidity, DHT.temperature);
+    sendReadings();
     relaySwitch();
-    break;
-  }
 }
 
 
@@ -114,42 +105,105 @@ void WebServer() {
           // character) and the line is blank, the http request has ended,
           // so you can send a reply
           if (c == '\n' && currentLineIsBlank) {
+
             // send a standard http response header
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
             client.println("Connection: close");  // the connection will be closed after completion of the response
             client.println("Refresh: 60");  // refresh the page automatically every 5 sec
             client.println();
+
             //and metadata...
             client.println("<!DOCTYPE HTML>");
             client.println("<html>");
             client.println("<head>");
             client.println("<title>Environment Live Times</title>");
             client.println("<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"http://arduino.cc/en/favicon.png\" />");
+
+            //and google chart script...
+            client.println("<script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>");
+            client.println("<script type=\"text/javascript\">");
+            client.println("  google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});");
+            client.println("  google.setOnLoadCallback(drawChart);");
+            client.println("  function drawChart() {");
+            client.println("    var data = google.visualization.arrayToDataTable([");
+            client.println("      ['Year', 'Sensor_1_Temp', 'Sensor_2_Temp', 'Sensor_3_Temp', 'Sensor_4_Temp'],");
+
+
+
+            // SELECT query for lookup value (1 row returned)
+            // Here we get a value from the database and use it.
+        /*    int temperature = 0;
+            char GETTEMP[];
+            sprintf(GETTEMP, "")
+            my_conn.cmd_query(QUERY_POP);
+            // We ignore the columns but we have to read them to get that data out of the queue
+            my_conn.get_columns();
+            // Now we read the rows.
+            row_values *row = NULL;
+            do {
+              row = my_conn.get_next_row();
+              // We use the first value returned in the row - population of NYC!
+              if (row != NULL) {
+                head_count = atol(row->values[0]);
+              }
+            } 
+            while (row != NULL);
+            // We're done with the buffers so Ok to clear them (and save precious memory).
+            my_conn.free_columns_buffer();
+            my_conn.free_row_buffer();
+            // Now, let's do something with the data.
+            Serial.print("NYC pop = ");
+            Serial.println(head_count);
+
+*/
+
+            /*
+             client.println("      ['2004',      23,         26,       41,       40],");
+             client.println("      ['2005',      23,         27,       40,       41],");
+             client.println("      ['2006',      22,         27,       39,       42],");
+             client.println("      ['2007',      23,         26,       38,       40]");
+             */
+
+
+            client.println("    ]);");
+            client.println("");
+            client.println("    var options = {");
+            client.println("      title: 'Sensor Readings'");
+            client.println("    };");
+            client.println("");
+            client.println("    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));");
+            client.println("");
+            client.println("    chart.draw(data, options);");
+            client.println("  }");
+            client.println("</script>");
+            client.println("");
+
             //and CSS style data...
             client.println("<style type=\"text/css\">");
             client.println("container {");
-            client.println("position: relative;");
-            client.println("width: 400px;");
-            client.println("margin-left:auto;");
-            client.println("margin-right:auto;");
+            client.println("  position: relative;");
+            client.println("  width: 400px;");
+            client.println("  margin-left:auto;");
+            client.println("  margin-right:auto;");
             client.println("}");
             client.println("reading {");
-            client.println("float: left;");
-            client.println("width: 100px;");
-            client.println("font-family: \"MS Serif\", \"New York\", serif;");
-            client.println("font-size: 12px;");
-            client.println("text-align: center;");
-            client.println("padding: 20px, 20px. 20px, 20px;");
+            client.println("  float: left;");
+            client.println("  width: 100px;");
+            client.println("  font-family: \"MS Serif\", \"New York\", serif;");
+            client.println("  font-size: 12px;");
+            client.println("  text-align: center;");
+            client.println("  padding: 20px, 20px. 20px, 20px;");
             client.println("}");
             client.println("reading h1 {");
-            client.println("font-family: Georgia, \"Times New Roman\", Times, serif;");
-            client.println("font-size: 14px;");
-            client.println("font-style: oblique;");
+            client.println("  font-family: Georgia, \"Times New Roman\", Times, serif;");
+            client.println("  font-size: 14px;");
+            client.println("  font-style: oblique;");
             client.println("}");
-
             client.println("</style>");
+
             client.println("</head>");
+
             //and body content.
             client.println("<body>");
             client.println("<container>");
@@ -190,7 +244,12 @@ void WebServer() {
             client.print(DHT.humidity);
             client.print("%.  <br /><br />   Temperature <br /> ");
             client.print(DHT.temperature);
-            client.println("C </reading>");    
+            client.println("C </reading>");  
+
+            client.println("<br /><br />");
+            client.println("<div id=\"chart_div\" style=\"width: 900px; height: 500px; position: absolute; top: 200px;\"></div>");
+
+
             client.println("</container>");
 
             client.println("</body>");
@@ -212,12 +271,7 @@ void WebServer() {
       // close the connection:
       client.stop();
     }
-    delay(1);
   }
-  if (previousScheduler == 4) {
-    previousScheduler = 0;
-  }
-  scheduler = previousScheduler + 1;
 }
 
 
@@ -225,24 +279,24 @@ void WebServer() {
 
 
 
-void sendReadings(int sensornum, int humidity, int temp) { 
+void sendReadings() { 
   //tell the serial monitor what we're doing.
-  /* char printf1[50];
-   sprintf(printf1, "TRYING TO INSERT SQL TO '%d', '%d', '%d'", sensornum, humidity, temp);
-   Serial.println(printf1); */
-  Serial.print("SQL INSERT ");
-  Serial.print(sensornum);
-
+  Serial.println(" SQL INSERT ");
+  
+  int sensorvalue[8];
+  //fill the sensorvalue array
+  for (int i=0; i < 4; i++) {
+    DHT.read11(pin_dht[i]);
+    sensorvalue[i*2] = DHT.humidity;
+    sensorvalue[(i*2)+1] = DHT.temperature;
+  }
   //build the query, correcting any variable usage/data type issues
   char SQL_SEND_READINGS[100];
-  sprintf(SQL_SEND_READINGS, "INSERT INTO iansmi9_ard.log VALUES (NULL, CURRENT_TIMESTAMP, '%d', '%d', '%d')", sensornum, humidity, temp);
+  sprintf(SQL_SEND_READINGS, "INSERT INTO iansmi9_ard.log VALUES (NULL, CURRENT_TIMESTAMP, '%d', '%d', '%d',  '%d', '%d', '%d',  '%d', '%d')", 
+        sensorvalue[0], sensorvalue[1], sensorvalue[2], sensorvalue[3], sensorvalue[4], sensorvalue[5], sensorvalue[6], sensorvalue[7]);
 
   /* run the SQL query */
   my_conn.cmd_query(SQL_SEND_READINGS);
-
-  //increment scheduler
-  previousScheduler = scheduler;
-  scheduler = 0;
 }
 
 
@@ -285,6 +339,8 @@ void relaySwitch() {
     Serial.println(temp);
   }
 }
+
+
 
 
 
